ef1bd085f90245b7a4cb1d2b93d067314020fce1
TheBaytur <bayturcodding@gmail.com>
2023-12-18 15:36:37 +0800

    Callback tasks

---
+++ src/EmployeeChain.java
@@ -0,0 +1,23 @@
+public class EmployeeChain {
+
+    private final Employee employee;
+    private EmployeeChain nextEmployeeChain;
+
+    public EmployeeChain(Employee employee) {
+        this.employee = employee;
+    }
+
+    public void doTask(Task task){
+        if (task.getStatus() == employee.getTaskStatus()){
+            employee.doTask(task);
+        } else if (nextEmployeeChain != null){
+            nextEmployeeChain.doTask(task);
+        } else {
+            throw new IllegalArgumentException("task can't be handled");
+        }
+    }
+
+    public void setNextEmployee(EmployeeChain nextEmployeeChain){
+        this.nextEmployeeChain = nextEmployeeChain;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TaskFactory factory = new TaskFactory();
+        TaskProgressCallback callback = new CallbackImpl(factory);
+        EmployeeChain chain = new EmployeeChain(new Designer(callback,"Alycia"));
+        EmployeeChain next = new EmployeeChain(new Programmer(callback,"John"));
+        EmployeeChain last = new EmployeeChain(new Tester(callback,"Steve"));
+        next.setNextEmployee(last);
+        chain.setNextEmployee(next);
+
+        while (true)
+            chain.doTask(factory.getTask());
+
     }
 }
